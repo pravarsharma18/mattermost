dfb5c8685eb64f46d76199e1f72c70270600c5b8
pravar <pravar.sharma@trootech.com>
2022-11-22 10:09:06 +0530

    updated sh file

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 ### Delete data from Mattermost and Zoho DB.
 
-python3 truncate_tables.py
+# python3 truncate_tables.py
 
 ### Create Zoho Tables in local DB
 
-# python3 create_zoho_tables.py
+python3 create_zoho_tables.py
 
 ### Get Data from zoho API and Insert in Local DB
 
@@ -17,6 +17,6 @@ python3 zoho_cliq_client.py
 ### Get Insert in Mattermost DB
 
 python3 mattermost_client.py
-# python3 mattermost_channel_client.py
+python3 mattermost_channel_client.py
 
 echo "<============================Data Migration Completed============================>"
