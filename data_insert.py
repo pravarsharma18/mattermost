ffb61bee4a5f05acf323dc3fd02d1d52cfa2b06f
Dipen Patel <dipen.patel@trootech.com>
2022-12-29 10:52:56 +0530

    get user using id

--- data_insert.py
+++ data_insert.py
@@ -5,7 +5,7 @@ from PIL import Image
 from decouple import config
 from datetime import datetime
 import time
-from sql import MatterSqlClient
+from sql import MatterSqlClient, ZohoSqlClient
 from utils import check_token_revoke_cliq, remove_punctions, generate_id, save_logs
 from zoho_cliq_client import ZohoClient
 import os
@@ -23,75 +23,78 @@ def image_data(channel_id, zoho_cliq_message, file, posts_keys, fileinfo_keys, i
         id_channel = channel_id['id']
         sender_name = remove_punctions(
             json.loads(zoho_cliq_message['sender'])['name'])
-        sender_id = MatterSqlClient.sql_get(
-            "users", 'id', f"username like '%{sender_name}%'")
-        if sender_id:
-            image_fileinfo_id = generate_id(26)
-            posts_id = generate_id(26)
+        user_email = ZohoSqlClient.sql_get("cliq_users", "email_id", f"id='{json.loads(zoho_cliq_message['sender'])['id']}'")
+        if user_email:
+            user_email_id = user_email[0]["email_id"]
+            sender_id = MatterSqlClient.sql_get('users', 'id', f"email='{user_email_id}'")
+            if sender_id:
+                image_fileinfo_id = generate_id(26)
+                posts_id = generate_id(26)
 
-            path = f"{date_folder}/teams/noteam/channels/{id_channel}/users/{sender_id[0]['id']}/{image_fileinfo_id}/"
-            file_path = f"{mattermost_base_path}{path}" 
-            type_images_fileinfo_values = [
-                image_fileinfo_id, sender_id[0]['id'], posts_id, zoho_cliq_message['time'], zoho_cliq_message['time'], 0, f'{path}{file["name"]}', f"{path}{'_thumb.'.join(file['name'].split('.'))}", f"{path}{'_preview.'.join(file['name'].split('.'))}", file['name'], 'jpeg', file['dimensions']['size'], file['type'], 0, 0, json.dumps(False), json.dumps(None), "", "", json.dumps(False)]
-            image_posts_values = [posts_id, zoho_cliq_message['time'], zoho_cliq_message['time'], 0, sender_id[0]['id'], id_channel, "", "", "", "", json.dumps(
-                {"disable_group_highlight": True}), "", json.dumps([]), json.dumps([image_fileinfo_id]), json.dumps(False), 0, json.dumps(False), json.dumps(None)]
-            pathlib.Path(file_path).mkdir(
-                parents=True, exist_ok=True)
+                path = f"{date_folder}/teams/noteam/channels/{id_channel}/users/{sender_id[0]['id']}/{image_fileinfo_id}/"
+                file_path = f"{mattermost_base_path}{path}" 
+                type_images_fileinfo_values = [
+                    image_fileinfo_id, sender_id[0]['id'], posts_id, zoho_cliq_message['time'], zoho_cliq_message['time'], 0, f'{path}{file["name"]}', f"{path}{'_thumb.'.join(file['name'].split('.'))}", f"{path}{'_preview.'.join(file['name'].split('.'))}", file['name'], 'jpeg', file['dimensions']['size'], file['type'], 0, 0, json.dumps(False), json.dumps(None), "", "", json.dumps(False)]
+                image_posts_values = [posts_id, zoho_cliq_message['time'], zoho_cliq_message['time'], 0, sender_id[0]['id'], id_channel, "", "", "", "", json.dumps(
+                    {"disable_group_highlight": True}), "", json.dumps([]), json.dumps([image_fileinfo_id]), json.dumps(False), 0, json.dumps(False), json.dumps(None)]
+                pathlib.Path(file_path).mkdir(
+                    parents=True, exist_ok=True)
+                
+                if img_counter == 30:
+                    img_counter = 1
+                    print("Files api for images throttling api please wait for 120 seconds.")
+                    time.sleep(120)
             
-            if img_counter == 30:
-                img_counter = 1
-                print("Files api for images throttling api please wait for 120 seconds.")
-                time.sleep(120)
-           
-            r = requests.get(
-                f"https://cliq.zoho.in/api/v2/files/{file['id']}", headers={
-                    "Authorization": f"Bearer {access_token}",
-                    "Content-Type": 'application/json'
-                })
-            
-            if r.status_code == 401:
-                if r.json()['code'] == "oauthtoken_invalid":
-                    new_access_token = check_token_revoke_cliq(r)
-                    os.environ.pop('ZOHO_CLIQ_API_KEY')
-                    os.environ['ZOHO_CLIQ_API_KEY'] = new_access_token
-                    r = requests.get(
-                        f"https://cliq.zoho.in/api/v2/files/{file['id']}", headers={
-                            "Authorization": f"Bearer {new_access_token}",
-                            "Content-Type": 'application/json'
-                        })
-            
-            if r.status_code == 400:
-                print(f"Not able to dowanload file: {file['id']}")
-                return
+                r = requests.get(
+                    f"https://cliq.zoho.in/api/v2/files/{file['id']}", headers={
+                        "Authorization": f"Bearer {access_token}",
+                        "Content-Type": 'application/json'
+                    })
+                
+                if r.status_code == 401:
+                    if r.json()['code'] == "oauthtoken_invalid":
+                        new_access_token = check_token_revoke_cliq(r)
+                        os.environ.pop('ZOHO_CLIQ_API_KEY')
+                        os.environ['ZOHO_CLIQ_API_KEY'] = new_access_token
+                        r = requests.get(
+                            f"https://cliq.zoho.in/api/v2/files/{file['id']}", headers={
+                                "Authorization": f"Bearer {new_access_token}",
+                                "Content-Type": 'application/json'
+                            })
+                
+                if r.status_code == 400:
+                    print(f"Not able to dowanload file: {file['id']}")
+                    return
 
-            r = r.content
+                r = r.content
 
-            with open(f"{file_path}{file['name']}", 'wb') as handler:
-                handler.write(r)
-            im = Image.open(f"{file_path}{file['name']}")
+                with open(f"{file_path}{file['name']}", 'wb') as handler:
+                    handler.write(r)
+                im = Image.open(f"{file_path}{file['name']}")
 
-            im.save(file_path + '_preview.'.join(file['name'].split('.')),
-                    optimize=True)
-            newsize = (120, 120)
-            im1 = im.resize(newsize)
-            im1.save(file_path + '_thumb.'.join(file['name'].split('.')), optimize=True)
+                im.save(file_path + '_preview.'.join(file['name'].split('.')),
+                        optimize=True)
+                newsize = (120, 120)
+                im1 = im.resize(newsize)
+                im1.save(file_path + '_thumb.'.join(file['name'].split('.')), optimize=True)
 
-            fileinfo_db = MatterSqlClient.sql_get("fileinfo", "name,createat,creatorid", f"name='{file['name']}' and createat='{zoho_cliq_message['time']}' and creatorid='{sender_id[0]['id']}'")
+                fileinfo_db = MatterSqlClient.sql_get("fileinfo", "name,createat,creatorid", f"name='{file['name']}' and createat='{zoho_cliq_message['time']}' and creatorid='{sender_id[0]['id']}'")
 
-            if fileinfo_db:
-                if fileinfo_db[0]['name'] != file['name'] and fileinfo_db[0]['createat'] != zoho_cliq_message['time'] and fileinfo_db[0]['creatorid'] != sender_id[0]['id']:
+                if fileinfo_db:
+                    if fileinfo_db[0]['name'] != file['name'] and fileinfo_db[0]['createat'] != zoho_cliq_message['time'] and fileinfo_db[0]['creatorid'] != sender_id[0]['id']:
+                        MatterSqlClient.sql_post(
+                            table_name="posts", attrs=posts_keys, values=image_posts_values)
+                        MatterSqlClient.sql_post(
+                            table_name="fileinfo", attrs=fileinfo_keys, values=type_images_fileinfo_values)
+                else:
                     MatterSqlClient.sql_post(
-                        table_name="posts", attrs=posts_keys, values=image_posts_values)
+                            table_name="posts", attrs=posts_keys, values=image_posts_values)
                     MatterSqlClient.sql_post(
-                        table_name="fileinfo", attrs=fileinfo_keys, values=type_images_fileinfo_values)
+                            table_name="fileinfo", attrs=fileinfo_keys, values=type_images_fileinfo_values)
             else:
-                MatterSqlClient.sql_post(
-                        table_name="posts", attrs=posts_keys, values=image_posts_values)
-                MatterSqlClient.sql_post(
-                        table_name="fileinfo", attrs=fileinfo_keys, values=type_images_fileinfo_values)
+                print(f"sender_name {sender_name} not found in users db mattermost")
         else:
             print(f"sender_name {sender_name} not found in users db mattermost")
-
     except Exception as e:
         save_logs(e)
 
@@ -106,81 +109,85 @@ def xlsx_data(channel_id, zoho_cliq_message, file, posts_keys, fileinfo_keys, ty
         sender_name = remove_punctions(
             json.loads(zoho_cliq_message['sender'])['name'])
         # try:
-        sender_id = MatterSqlClient.sql_get("users", "id", f"username like '%{sender_name}%'")
-        if sender_id:
-            xml_fileinfo_id = generate_id(26)
-            posts_id = generate_id(26)
-            path = f"{date_folder}/teams/noteam/channels/{id_channel}/users/{sender_id[0]['id']}/{xml_fileinfo_id}/"
-            file_path = f"{mattermost_base_path}{path}" 
+        user_email = ZohoSqlClient.sql_get("cliq_users", "email_id", f"id='{json.loads(zoho_cliq_message['sender'])['id']}'")
+        if user_email:
+            user_email_id = user_email[0]["email_id"]
+            sender_id = MatterSqlClient.sql_get('users', 'id', f"email='{user_email_id}'")
+            if sender_id:
+                xml_fileinfo_id = generate_id(26)
+                posts_id = generate_id(26)
+                path = f"{date_folder}/teams/noteam/channels/{id_channel}/users/{sender_id[0]['id']}/{xml_fileinfo_id}/"
+                file_path = f"{mattermost_base_path}{path}" 
 
-            if type == "application/x-ooxml":
-                extension = file['name'].split('.')[-1]
-                minetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            elif type == "application/pdf":
-                extension = file['name'].split('.')[-1]
-                minetype = type
-            elif type == "application/zip":
-                extension = file['name'].split('.')[-1]
-                minetype = type
-            elif type == "text/plain":
-                extension = file['name'].split('.')[-1]
-                minetype = type
+                if type == "application/x-ooxml":
+                    extension = file['name'].split('.')[-1]
+                    minetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                elif type == "application/pdf":
+                    extension = file['name'].split('.')[-1]
+                    minetype = type
+                elif type == "application/zip":
+                    extension = file['name'].split('.')[-1]
+                    minetype = type
+                elif type == "text/plain":
+                    extension = file['name'].split('.')[-1]
+                    minetype = type
 
-            type_xml_fileinfo_values = [
-                xml_fileinfo_id, sender_id[0]['id'], posts_id, zoho_cliq_message['time'], zoho_cliq_message['time'], 0, f'{path}{file["name"]}', "", "", file['name'], extension, file['dimensions']['size'], minetype, 0, 0, json.dumps(False), json.dumps(None), "", "", json.dumps(False)]
-            xml_posts_values = [posts_id, zoho_cliq_message['time'], zoho_cliq_message['time'], 0, sender_id[0]['id'], id_channel, "", "", "", "", json.dumps(
-                {"disable_group_highlight": True}), "", json.dumps([]), json.dumps([xml_fileinfo_id]), json.dumps(False), 0, json.dumps(False), json.dumps(None)]
+                type_xml_fileinfo_values = [
+                    xml_fileinfo_id, sender_id[0]['id'], posts_id, zoho_cliq_message['time'], zoho_cliq_message['time'], 0, f'{path}{file["name"]}', "", "", file['name'], extension, file['dimensions']['size'], minetype, 0, 0, json.dumps(False), json.dumps(None), "", "", json.dumps(False)]
+                xml_posts_values = [posts_id, zoho_cliq_message['time'], zoho_cliq_message['time'], 0, sender_id[0]['id'], id_channel, "", "", "", "", json.dumps(
+                    {"disable_group_highlight": True}), "", json.dumps([]), json.dumps([xml_fileinfo_id]), json.dumps(False), 0, json.dumps(False), json.dumps(None)]
 
 
-            pathlib.Path(file_path).mkdir(
-                parents=True, exist_ok=True)
-            
-            if xlsx_counter == 30:
-                xlsx_counter = 1
-                print("Files api for xlsx throttling api please wait for 120 seconds.")
-                time.sleep(120)
+                pathlib.Path(file_path).mkdir(
+                    parents=True, exist_ok=True)
+                
+                if xlsx_counter == 30:
+                    xlsx_counter = 1
+                    print("Files api for xlsx throttling api please wait for 120 seconds.")
+                    time.sleep(120)
 
-            r = requests.get(
-                f"https://cliq.zoho.in/api/v2/files/{file['id']}", headers={
-                    "Authorization": f"Bearer {access_token}",
-                    "Content-Type": 'application/json'
-                })
-            
-            if r.status_code == 401:
-                if r.json()['code'] == "oauthtoken_invalid":
-                    new_access_token = check_token_revoke_cliq(r)
-                    os.environ.pop('ZOHO_CLIQ_API_KEY')
-                    os.environ['ZOHO_CLIQ_API_KEY'] = new_access_token
-                    r = requests.get(
-                        f"https://cliq.zoho.in/api/v2/files/{file['id']}", headers={
-                            "Authorization": f"Bearer {new_access_token}",
-                            "Content-Type": 'application/json'
-                        })
-            
-            if r.status_code == 400:
-                print(f"Not able to dowanload file: {file['id']}")
-                return
+                r = requests.get(
+                    f"https://cliq.zoho.in/api/v2/files/{file['id']}", headers={
+                        "Authorization": f"Bearer {access_token}",
+                        "Content-Type": 'application/json'
+                    })
+                
+                if r.status_code == 401:
+                    if r.json()['code'] == "oauthtoken_invalid":
+                        new_access_token = check_token_revoke_cliq(r)
+                        os.environ.pop('ZOHO_CLIQ_API_KEY')
+                        os.environ['ZOHO_CLIQ_API_KEY'] = new_access_token
+                        r = requests.get(
+                            f"https://cliq.zoho.in/api/v2/files/{file['id']}", headers={
+                                "Authorization": f"Bearer {new_access_token}",
+                                "Content-Type": 'application/json'
+                            })
+                
+                if r.status_code == 400:
+                    print(f"Not able to dowanload file: {file['id']}")
+                    return
 
-            r = r.content
+                r = r.content
 
-            with open(f"{file_path}{file['name']}", 'wb') as f:
-                f.write(r)
-            fileinfo_db = MatterSqlClient.sql_get("fileinfo", "name,createat,creatorid", f"name='{file['name']}' and createat='{zoho_cliq_message['time']}' and creatorid='{sender_id[0]['id']}'")
+                with open(f"{file_path}{file['name']}", 'wb') as f:
+                    f.write(r)
+                fileinfo_db = MatterSqlClient.sql_get("fileinfo", "name,createat,creatorid", f"name='{file['name']}' and createat='{zoho_cliq_message['time']}' and creatorid='{sender_id[0]['id']}'")
 
-            if fileinfo_db:
-                if fileinfo_db[0]['name'] != file['name'] and fileinfo_db[0]['createat'] != zoho_cliq_message['time'] and fileinfo_db[0]['creatorid'] != sender_id[0]['id']:
+                if fileinfo_db:
+                    if fileinfo_db[0]['name'] != file['name'] and fileinfo_db[0]['createat'] != zoho_cliq_message['time'] and fileinfo_db[0]['creatorid'] != sender_id[0]['id']:
+                        MatterSqlClient.sql_post(
+                            table_name="posts", attrs=posts_keys, values=xml_posts_values)
+                        MatterSqlClient.sql_post(
+                            table_name="fileinfo", attrs=fileinfo_keys, values=type_xml_fileinfo_values)
+                else:
                     MatterSqlClient.sql_post(
-                        table_name="posts", attrs=posts_keys, values=xml_posts_values)
+                            table_name="posts", attrs=posts_keys, values=xml_posts_values)
                     MatterSqlClient.sql_post(
                         table_name="fileinfo", attrs=fileinfo_keys, values=type_xml_fileinfo_values)
             else:
-                MatterSqlClient.sql_post(
-                        table_name="posts", attrs=posts_keys, values=xml_posts_values)
-                MatterSqlClient.sql_post(
-                    table_name="fileinfo", attrs=fileinfo_keys, values=type_xml_fileinfo_values)
+                print(f"sender_name {sender_name} not found in users db mattermost")
         else:
             print(f"sender_name {sender_name} not found in users db mattermost")
-
     except Exception as e:
         save_logs(e)
 
