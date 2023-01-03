540d59fca46d55192a3d1c470e43038b17fe3030
Dipen Patel <dipen.patel@trootech.com>
2023-01-03 18:18:31 +0530

    remove chat_id dependancy

--- mattermost_channel_client.py
+++ mattermost_channel_client.py
@@ -31,24 +31,16 @@ class MattermostClient:
 
     def insert_channels(self):
             keys = ['id', 'createat', 'updateat', 'deleteat', 'teamid', 'type', 'displayname', 'name',
-                    'header', 'purpose', 'lastpostat', 'totalmsgcount', 'extraupdateat', 'creatorid', 'totalmsgcountroot', 'lastrootpostat', 'chat_id']
+                    'header', 'purpose', 'lastpostat', 'totalmsgcount', 'extraupdateat', 'creatorid', 'totalmsgcountroot', 'lastrootpostat']
             teams = MatterSqlClient.sql_get("teams", "id")
             channels = ZohoSqlClient.sql_get("cliq_channels")
-            try:
-                # To get the channel id in the next method
-                # Deleted in the next method.
-                MatterSqlClient.add_column(
-                    'channels', 'chat_id', 'varchar(255)')
-            except:
-                pass
+
             for team in teams:
                 for channel in channels:
                     try:
                         display_channel_name = channel.get('name')
                         channel_name = remove_punctions(display_channel_name).replace(".", "-")
-                        chat_id = []
-                        if channel_name:
-                            chat_id = ZohoSqlClient.sql_get('cliq_channels', 'chat_id', f"name like '%{display_channel_name}%'" )
+                        chat_id = channel["chat_id"]
                         if chat_id:                        
                             creator_id = MatterSqlClient.sql_get(
                                 "users", "id", f"username='{remove_punctions(channel['creator_name'])}'")
@@ -56,8 +48,8 @@ class MattermostClient:
                                 values = [
                                     self.generate_id(26), self.get_timestamp_from_date(channel.get('creation_time')), 
                                     self.get_timestamp_from_date(channel.get('creation_time')), 0, team['id'],"O", display_channel_name, 
-                                    channel_name, "", "", self.get_timestamp_from_date(channel.get('creation_time')), 
-                                    channel.get('total_message_count'), 0, creator_id[0]['id'], 0, 0, chat_id[0]['chat_id']]
+                                    channel_name, "", chat_id[0]['chat_id'], self.get_timestamp_from_date(channel.get('creation_time')), 
+                                    channel.get('total_message_count'), 0, creator_id[0]['id'], 0, 0]
                                 channels = MatterSqlClient.sql_get("channels", "teamid,name", f"teamid='{team['id']}' and name='{channel_name}'")
                                 if channels:
                                     if channels[0]['teamid'] != team['id'] and channels[0]['name'] != channel_name:
@@ -78,7 +70,7 @@ class MattermostClient:
 
     def insert_chats(self):
             channel_keys = ['id', 'createat', 'updateat', 'deleteat', 'teamid', 'type', 'displayname', 'name',
-                            'header', 'purpose', 'lastpostat', 'totalmsgcount', 'extraupdateat', 'creatorid', 'shared', 'totalmsgcountroot', 'lastrootpostat', 'chat_id']
+                            'header', 'purpose', 'lastpostat', 'totalmsgcount', 'extraupdateat', 'creatorid', 'shared', 'totalmsgcountroot', 'lastrootpostat']
             prefrence_keys = MatterSqlClient.get_columns('preferences')
             # team = MatterSqlClient.sql_get("teams", "id")
             chats = ZohoSqlClient.sql_get("cliq_chats")
@@ -104,14 +96,14 @@ class MattermostClient:
                         if creator_id:
                             channel_values = [
                                 self.generate_id(26), int(float(chat['creation_time'])), int(float(chat['last_modified_time'])), 0, "",
-                                "D", "", rec_ids_str, "", "", int(float(chat['last_modified_time'])), 0, 0, creator_id[0]['id'], 
-                                json.dumps(False), 0, int(float(chat['last_modified_time'])), chat['chat_id']]
+                                "D", "", rec_ids_str, "", chat['chat_id'], int(float(chat['last_modified_time'])), 0, 0, creator_id[0]['id'], 
+                                json.dumps(False), 0, int(float(chat['last_modified_time']))]
                             channel_values_reverse = [
                                 self.generate_id(26), int(float(chat['creation_time'])), int(float(chat['last_modified_time'])), 0, "",
-                                "D", "", rec_ids_str_reverse, "", "", int(float(chat['last_modified_time'])), 0, 0, creator_id[0]['id'], 
-                                json.dumps(False), 0, int(float(chat['last_modified_time'])), chat['chat_id']]
+                                "D", "", rec_ids_str_reverse, "", chat['chat_id'], int(float(chat['last_modified_time'])), 0, 0, creator_id[0]['id'], 
+                                json.dumps(False), 0, int(float(chat['last_modified_time']))]
 
-                            channels_db = MatterSqlClient.sql_get("channels", "name,chat_id", f"name='{rec_ids_str}'")
+                            channels_db = MatterSqlClient.sql_get("channels", "name,purpose", f"name='{rec_ids_str}' and purpose='{chat['chat_id']}'")
                             channel_id = ""
                             if channels_db:
                                 if channels_db[0]['name'] != rec_ids_str:
@@ -120,9 +112,9 @@ class MattermostClient:
                             else:
                                 channel_id = MatterSqlClient.sql_post(
                                         table_name='channels', attrs=channel_keys, values=channel_values, returning='id')
-                            channels_db_reverse = MatterSqlClient.sql_get("channels", "name,chat_id", f"name='{rec_ids_str_reverse}' and chat_id='{chat['chat_id']}'")
+                            channels_db_reverse = MatterSqlClient.sql_get("channels", "name,purpose", f"name='{rec_ids_str_reverse}' and purpose='{chat['chat_id']}'")
                             if channels_db_reverse:
-                                if channels_db_reverse[0]['name'] != rec_ids_str and channels_db_reverse[0]['chat_id'] != chat['chat_id']:
+                                if channels_db_reverse[0]['name'] != rec_ids_str_reverse:
                                     channel_id_reverse = MatterSqlClient.sql_post(
                                         table_name='channels', attrs=channel_keys, values=channel_values_reverse, returning='id')
                             else:
@@ -190,15 +182,13 @@ class MattermostClient:
     def insert_posts(self):
             posts_keys = MatterSqlClient.get_columns("posts")
             fileinfo_keys = MatterSqlClient.get_columns('fileinfo')
-            # zoho_cliq_messages = ZohoSqlClient.sql_get('cliq_messages')
             mm_channels = MatterSqlClient.sql_get('channels')
             text = 1
             file_counter = 1
             for mm_channel in mm_channels:
-                zoho_cliq_messages = ZohoSqlClient.sql_get('cliq_messages', params=f"chat_id='{mm_channel.get('chat_id')}' and is_processed = false")
+                zoho_cliq_messages = ZohoSqlClient.sql_get('cliq_messages', params=f"chat_id='{mm_channel.get('purpose')}' and is_processed = false")
                 for zoho_cliq_message in zoho_cliq_messages:
                     try:
-                        # if mm_channel.get('chat_id') == zoho_cliq_message.get('chat_id'):
                         if zoho_cliq_message['type'] == 'text':
                             user_email = ZohoSqlClient.sql_get("cliq_users", "email_id", f"id='{json.loads(zoho_cliq_message['sender'])['id']}'")
                             if user_email:
@@ -209,12 +199,7 @@ class MattermostClient:
                                     posts_db = MatterSqlClient.sql_get("posts", "channelid,userid", f"channelid='{mm_channel['id']}' and userid='{user_id[0]['id']}' and message='{replace_escape_characters(json.loads(zoho_cliq_message['content'])['text'])}' and createat={zoho_cliq_message['time']}")
                                     type_text_values = [self.generate_id(
                                         26), zoho_cliq_message['time'], zoho_cliq_message['time'], 0, user_id[0]['id'], mm_channel['id'], "", "", json.loads(zoho_cliq_message['content'])['text'], "", json.dumps({"disable_group_highlight": True}), "", json.dumps([]), json.dumps([]), json.dumps(False), 0, json.dumps(False), json.dumps(None)]
-                                    if posts_db:
-                                        if posts_db[0]['channelid'] != mm_channel['id'] and posts_db[0]['userid'] != user_id[0]['id']:
-                                            text +=1
-                                            MatterSqlClient.sql_post(
-                                                table_name='posts', attrs=posts_keys, values=type_text_values)
-                                    else:
+                                    if not posts_db:
                                         text +=1
                                         MatterSqlClient.sql_post(
                                                 table_name='posts', attrs=posts_keys, values=type_text_values)
@@ -231,12 +216,10 @@ class MattermostClient:
                             else:
                                 xlsx_data(mm_channel, zoho_cliq_message, file, posts_keys, fileinfo_keys, file['type'], file_counter)
                         
-                        ZohoSqlClient.sql_update(table_name="cliq_messages", set="is_processed = true", where=f"chat_id = '{mm_channel.get('chat_id')}'")
+                        ZohoSqlClient.sql_update(table_name="cliq_messages", set="is_processed = true", where=f"chat_id = '{mm_channel.get('purpose')}'")
                     except Exception as e:
                         print(f"Exception in inserting posts:  {zoho_cliq_message['chat_id']}")
                         save_logs(e)
-                
-            MatterSqlClient.delete_column('channels', 'chat_id')
             # print("text", text)
             # print("img", img)
             # print("xl", xl)
